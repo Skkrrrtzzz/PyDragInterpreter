c819080f6cc3445290fed7beaa3c9631323b5d1e
Skkrrrtzz <kertzhenrich.gajete@cvsu.edu.ph>
2022-05-17 17:01:08 +0800

    commit

--- app/src/main/java/com/python/pydraginterpreter/MainDrag.java
+++ app/src/main/java/com/python/pydraginterpreter/MainDrag.java
@@ -1,21 +1,32 @@
 package com.python.pydraginterpreter;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainDrag extends AppCompatActivity {
     //initializing variables
     EditText editTextSampleCode;
-    TextView output;
+    TextView output,textDragHere;
     EditText CodeArea;
     Button Run;
+    String ForLoopCode, IfElseCode;
+    TextInputLayout textInputLayout;
+    AutoCompleteTextView autoCompleteTextView;
+    TextView textViewDraggable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,9 @@ public class MainDrag extends AppCompatActivity {
         output = findViewById(R.id.output);
         CodeArea = findViewById(R.id.codearea);
         Run = findViewById(R.id.run);
+        textInputLayout = findViewById(R.id.menu_dropdown);
+        autoCompleteTextView = findViewById(R.id.dropDownItems);
+        textViewDraggable = findViewById(R.id.tvItemSelectedDraggable);
 
         //finding the ID of the Edit Text(Coding Area) kungsan mapupunta ang sample code
         editTextSampleCode = findViewById(R.id.codearea);
@@ -58,6 +72,81 @@ public class MainDrag extends AppCompatActivity {
             }
         });
 
+        String[] itemsList = {"For Loop", "If Else",};
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<>(MainDrag.this, R.layout.item_list, itemsList);
+        autoCompleteTextView.setAdapter(itemAdapter);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                textViewDraggable.setText((String) parent.getItemAtPosition(position));
+
+                if (textViewDraggable.getText() == "For Loop") {
+                    textViewDraggable.setTag("For Loop");
+                }
+                else if (textViewDraggable.getText() == "If Else") {
+                    textViewDraggable.setTag("If Else");
+                }
+            }
+        });
+
+
+        //START | DRAG N DROP
+
+        textDragHere = (EditText) findViewById(R.id.codearea);
+
+        textViewDraggable.setOnLongClickListener(longClickListener);
+
+        textDragHere.setOnDragListener(dragListener);
+
+        //Sample prepared code
+        ForLoopCode = "\nfruits = ['apple', 'banana', 'cherry']\nfor x in fruits\n  print(x)";
+
+        IfElseCode = "\na = 33\n b = 200\n if b > a:\n  print('b is greater than a')";
 
     }
+
+    //PARA SA PANG HOLD NG TEXT
+    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data, myShadowBuilder, v, 0);
+            return true;
+        }
+    };
+
+    //PARA SA PANDRAG
+    final View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+            int dragEvent = event.getAction();
+            final View view = (View) event.getLocalState();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+                case DragEvent.ACTION_DROP:
+                    if (view.getTag() == "For Loop") {
+                        ClipData.Item item = event.getClipData().getItemAt(0);
+                        CharSequence currentData = item.getText();
+                        textDragHere.append(currentData.toString() + ForLoopCode);
+                    }
+                    if (view.getTag() == "If Else") {
+                        ClipData.Item item = event.getClipData().getItemAt(0);
+                        CharSequence currentData = item.getText();
+                        textDragHere.append(currentData.toString() + IfElseCode);
+                    }
+
+
+                    break;
+            }
+
+            return true;
+        }
+
+        //END | DRAG N DROP
+    };
 }
